a0753c7cf64080667bf78e8a889f62b0a0619ae0
Izayoi-R <59307807+Izayoi-R@users.noreply.github.com>
2024-08-19 01:55:12 +0900

    サインイン/アップページを用意する

---
+++ frontend_Project/node/frontend/src/components/template/SignIn.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const SignIn = () => {
+    return (
+        <div>
+            <p>サインインページです</p>
+            <p><a href="/SignUp">サインアップはこちら</a></p>
+        </div>
+    )
+};
+
+export default SignIn;
---
+++ frontend_Project/node/frontend/src/components/template/SignUp.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const SignUp = () => {
+    return (
+        <div>
+            <p>サインアップページです</p>
+            <p><a href="/">トップに戻る</a></p>
+        </div>
+    )
+};
+
+export default SignUp;
