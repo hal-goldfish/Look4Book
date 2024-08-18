48cf6646046d4948201a47bcfd7ba1b87cc6e2f8
Izayoi-R <59307807+Izayoi-R@users.noreply.github.com>
2024-08-18 23:57:17 +0900

    'Nextを導入'

---
+++ frontend_Project/node/frontend/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Top = () => {
+    return (
+        <div>
+            <p>トップページです</p>
+            <p><a href="/TempView">TempView</a></p>
+            <p><a href="/TempView2">TempView2</a></p>
+        </div>
+    );
+};
+
+export default Top;
