5b0bf566065b5def1f4311aff765602c889dd691
takagiharuki <takagi_haruki@l2d-partner.com>
2024-08-18 19:15:08 +0900

    add 書影をDBに保存

--- backend_Project/backend/api/get_data.py
+++ backend_Project/backend/api/get_data.py
@@ -1,6 +1,9 @@
 from api.models import User, Book
 
+import os
 import requests
+from io import BytesIO
+from django.core.files.base import ContentFile
 
 
 def get_and_save_data(isbn, user_id):
@@ -40,11 +43,25 @@ def get_and_save_data(isbn, user_id):
 		publisher += text[idx+i]
 
 
-
 	book = Book(ISBN = isbn,
-				title = title,
-				author = author,
-				publisher = publisher)
+		title = title,
+		author = author,
+		publisher = publisher, )
+	
+	book.save()
+
+	
+	# 書影
+	url_image = "https://ndlsearch.ndl.go.jp/thumbnail/" + isbn + ".jpg"
+
+	response = requests.get(url_image)
+	if response.headers['Content-Type'] == "image/jpeg": # 書影がある
+		path = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/images/" + str(book.id) + ".jpg"
+		with open(path, 'wb') as file:
+			file.write(response.content)
+		book.book_cover = path
+
+
 	book.save()
 
 	user = User.objects.get(id=user_id)
@@ -53,49 +70,3 @@ def get_and_save_data(isbn, user_id):
 
 	return True
 
-
-# Book 型を返す
-def get_data(isbn):
-	url = "https://ndlsearch.ndl.go.jp/api/sru?operation=searchRetrieve&query=isbn%3d\"" + isbn + "\""
-	data = requests.get(url)
-	text = data.text
-
-	if text.find("Record does not exist") != -1:
-		return None
-	
-	# タイトル
-	idx = text.find("title")+9
-	title = ""
-	for i in range(100):
-		if text[idx+i] == '&':
-			break
-		title += text[idx+i]
-	
-	# 著者
-	idx = text.find("creator")
-	author = ""
-	if idx == -1:
-		pass
-	else:
-		idx += 11
-		for i in range(100):
-			if text[idx+i] == '&':
-				break
-			author += text[idx+i]
-
-	# 出版社
-	idx = text.find("publisher")+13
-	publisher = ""
-	for i in range(100):
-		if text[idx+i] == '&':
-			break
-		publisher += text[idx+i]
-
-
-	book = Book(ISBN = isbn,
-				title = title,
-				author = author,
-				publisher = publisher)
-	
-
-	return book
--- backend_Project/backend/api/models.py
+++ backend_Project/backend/api/models.py
@@ -9,7 +9,7 @@ class Book(models.Model):
 	author = models.CharField(max_length=20, null=True)
 	publisher = models.CharField(max_length=20, null=True)
 	overview = models.TextField(null=True)
-	books_cover = models.ImageField(null=True)
+	book_cover = models.ImageField(upload_to = 'images/', null=True)
 
 	def __str__(self):
 		return self.title
