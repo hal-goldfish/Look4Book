d2c1a83d8e1eeddd76e380fec839ef55feacdc73
Izayoi-R <59307807+Izayoi-R@users.noreply.github.com>
2024-08-21 16:20:27 +0900

    localStorageをsessionStorageに変更，IDからUser情報を取得

---
+++ frontend_Project/node/frontend/src/functions/getUser.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { User } from "../types/User";
+import { GET_USER_LIST } from "../consts/API";
+
+export async function getUser(userId: number): Promise<User | null> {
+    let apiIsSuccess = true;
+    const res = await axios.get(GET_USER_LIST+String(userId)).catch(()=>{
+        apiIsSuccess = false;
+    });
+    if(apiIsSuccess){
+        return {
+            id: userId,
+            name: String(res.data['name']),
+            role: 'user',
+        };
+    }else{
+        return null;
+    }
+}
