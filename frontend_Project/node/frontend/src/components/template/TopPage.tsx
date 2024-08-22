2fb4041b800e4f9f6ddb1110cb488e28ebdc5c43
Izayoi-R <59307807+Izayoi-R@users.noreply.github.com>
2024-08-22 22:23:24 +0900

    トップページ微修正

--- frontend_Project/node/frontend/src/components/template/TopPage.tsx
+++ frontend_Project/node/frontend/src/components/template/TopPage.tsx
@@ -14,19 +14,21 @@ export const TopPage = () => {
     return (
         <Box bgImage='./images/topPageBG.jpg' bgSize='cover' h='100vh' overflow='hidden'>
             <VStack>
-                <HStack w='100%' spacing='24px' pr='24px' py='8px' bgColor='lightgreen' justify='right'>
-                    <Button variant='link' colorScheme="teal" onClick={onClickSignup}>
+                <HStack w='100%' spacing='24px' pr='24px' py='8px' justify='right'>
+                    <Button variant='ghost' color="black" onClick={onClickSignup} bgColor='rgba(200,0,0,0.5)'>
                         <Text fontSize='x-large'>sign up</Text>
                     </Button>
-                    <Button variant='link' colorScheme="teal" onClick={onClickLogin}>
+                    <Button variant='ghost' color="black" onClick={onClickLogin} bgColor='rgba(200,0,0,0.5)'>
                         <Text fontSize='x-large'>log in</Text>
                     </Button>
                 </HStack>
                 <Box display='flex' justifyContent='left' w='100%' ml='80px' mt='64px'>
-                    <Heading><Text px='16px' bgColor='lightblue' fontSize='xxx-large' color='teal'>Look4Book</Text></Heading>
+                    <Heading>
+                        <Text px='16px' bgColor='rgba(200,0,0,0.5)' fontSize='xxx-large' color='black'>Look4Book</Text>
+                    </Heading>
                 </Box>
                 <Box display='flex' justifyContent='center' w='100%' mr='25%'>
-                    <Text bgColor='aquamarine' fontSize='x-large'>本の森で、暮らすということ</Text>
+                    <Text bgColor='rgba(200,0,0,0.5)' fontSize='x-large' color='black'>本の森で、暮らすということ</Text>
                 </Box>
             </VStack>
         </Box>
