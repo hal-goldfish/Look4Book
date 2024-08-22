5faf21f56b3da863bc7c444c36ceadb7507966c2
Izayoi-R <59307807+Izayoi-R@users.noreply.github.com>
2024-08-23 03:36:57 +0900

    共通背景画像を設定

--- frontend_Project/node/frontend/src/pages/UserProfile.tsx
+++ frontend_Project/node/frontend/src/pages/UserProfile.tsx
@@ -1,21 +1,11 @@
 import React from "react";
 import {UserProfile as UserProfileTemplate } from "../components/template/UserProfile";
 import { RouteProtecter } from "../components/RouteProtecter";
-import { Header } from "../components/orgnism/Header";
-import { Box, HStack } from "@chakra-ui/react";
-import { SideMenu } from "../components/orgnism/SideMenu";
-import { menuListWhenSignIn } from "../consts/SideMenu";
 
 export const UserProfile = () => {
     return (
         <>
-            <Header subText='プロフィール'/>
-            <HStack alignItems='flex-start'>
-                <SideMenu menuList={menuListWhenSignIn}/>
-                <Box w='100%' h='90vh' overflow='auto'>
-                    <RouteProtecter component={<UserProfileTemplate/>} allowedRoles={['admin','manager','user']}/>
-                </Box>
-            </HStack>
+            <RouteProtecter component={<UserProfileTemplate/>} allowedRoles={['admin','manager','user']}/>
         </>
     );
 };
