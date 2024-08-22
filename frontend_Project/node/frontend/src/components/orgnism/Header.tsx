a4565a137cd3b90b6a4b748b086f1e062851be55
Izayoi-R <59307807+Izayoi-R@users.noreply.github.com>
2024-08-23 04:39:23 +0900

    Headerコンポーネントを修正

--- frontend_Project/node/frontend/src/components/orgnism/Header.tsx
+++ frontend_Project/node/frontend/src/components/orgnism/Header.tsx
@@ -1,23 +1,26 @@
-import { HStack, Text } from "@chakra-ui/react";
-import React from "react";
+import { HStack, Spacer, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
 
 type HeaderProps = {
-    text?: String;
-    subText: String;
+    title: String;
+    userName: String;
 };
 
 export const Header = ({
-    text='Look4Book',
-    subText,
+    title,
+    userName,
 }: HeaderProps) => {
+    const [curTime, setCurTime] = useState<Date>(new Date());
+    setInterval(()=>{setCurTime(new Date())}, 30*1000);
     return (
-        <HStack
-        top={0}
-        width="100%"
-        height="10vh"
-        bg="lightgreen">
-            <Text fontSize='24px' fontWeight="bold">{text}</Text>
-            <Text fontSize='18px'> - {subText}</Text>
+        <HStack w="100%" h="10vh" bgColor='rgba(200,200,200,0.8)' px='10%'>
+            <Text fontSize='18px' fontWeight="bold">
+                {curTime.toLocaleDateString("ja-JP", {year: "numeric", month: "2-digit", day: "2-digit", hour: "2-digit", minute: "2-digit"})}
+            </Text>
+            <Spacer/>
+            <Text fontSize='18px'>{title}</Text>
+            <Spacer/>
+            <Text fontSize='18px'>{userName}</Text>
         </HStack>
     );
 };
