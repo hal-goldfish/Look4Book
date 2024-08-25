b57ce302bff670f91ac79a93760cd7d1fa5aefff
Izayoi-R <59307807+Izayoi-R@users.noreply.github.com>
2024-08-25 09:01:18 +0900

    ユーザープロフィール画面作成

--- frontend_Project/node/frontend/src/components/template/UserProfile.tsx
+++ frontend_Project/node/frontend/src/components/template/UserProfile.tsx
@@ -1,22 +1,52 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useAuthUserContext } from "../../providers/AuthUser";
-import { Box, Flex, HStack, Text, VStack } from "@chakra-ui/react";
+import { Box, Button, Flex, Grid, HStack, Text, Tooltip, VStack } from "@chakra-ui/react";
 import { Header } from "../orgnism/Header";
 import { commonBG } from "../../consts/IMAGE";
+import { SimpleCard } from './../orgnism/SimpleCard';
+import { STATES } from "../../consts/States";
+import { CATEGORIES } from "../../consts/Categories";
+import { Refresh } from "@mui/icons-material";
 
 const UserProfile = () => {
-    const { user } = useAuthUserContext();
+    const { user, fetchUser } = useAuthUserContext();
     return (
-        <VStack>
-            <HStack>
-                <Text>ユーザー名</Text>
-                <Text>{user?.name}</Text>
-            </HStack>
-            <HStack>
-                <Text>ランク</Text>
-                <Text>{user?.role}</Text>
-            </HStack>
-        </VStack>
+        <>
+            { (!!user) ?
+                <VStack w='100%' maxH='100%' overflow='auto'>
+                    <HStack>
+                        <Flex h='10%' px={5} bg='rgba(50,50,200,0.5)'>
+                            <Text fontSize='xx-large' fontWeight='bold'>
+                                {(user?.name||'') + '　のダッシュボード'}
+                            </Text>
+                        </Flex>
+                        <Tooltip label='更新'>
+                            <Button variant='ghost' onClick={fetchUser}>
+                                    <Refresh/>
+                            </Button>
+                        </Tooltip>
+                    </HStack>
+                    <VStack maxH='90%' p={2} bg='rgba(200,100,0,0.5)' overflow='auto'>
+                        <HStack>
+                            {user.stateCount.map((value, idx) => {
+                                const label = STATES.state[idx];
+                                return (
+                                    <SimpleCard label={label} value={String(value)} size='large'/>
+                                );
+                            })}
+                        </HStack>
+                        <Grid templateColumns='repeat(4, 1fr)' gap={2}>
+                            {user.categoryCount.map((value, idx) => {
+                                const label = CATEGORIES.categories[idx];
+                                return (
+                                    <SimpleCard label={label} value={String(value)} size='normal'/>
+                                );
+                            })}
+                        </Grid>
+                    </VStack>
+                </VStack>
+            : null}
+        </>
     );
 }
 
@@ -25,7 +55,7 @@ export const UserProfileTemplate = () => {
         <VStack bgImage={commonBG} bgSize='cover' bgRepeat='no-repeat' h='100vh' overflow='hidden'>
             <Header curPage='プロフィール'/>
             <Flex w='100%' h='90vh' alignItems='center' justify='center'>
-                <Box alignItems='center' w='90%' maxW='90%' h='90%' bgColor='rgba(255,255,255,0.5)' p='8px' overflow='auto'>
+                <Box alignItems='center' w='90%' maxW='90%' h='90%' bgColor='rgba(255,255,255,0.5)' p='8px'>
                     <UserProfile/>
                 </Box>
             </Flex>
